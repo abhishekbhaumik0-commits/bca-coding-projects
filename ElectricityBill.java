9f0c6cabcec587856cd224739d4ff42693091ec0
abhishekbhaumik0-commits <abhishekbhaumik0@gmail.com>
2026-05-18 12:43:37 +0530

    Update ElectricityBill.java

--- ElectricityBill.java
+++ ElectricityBill.java
@@ -1,1 +1,26 @@
+import java.util.Scanner;
+
+class Main{
+    static double electricityBillpayment(double U){
+    double bill;
+    if(U<=100){
+        bill = 5 * U; 
+    }
+    else if(U<=200){
+        bill = 500 + 7 * (U-100);
+    }
+    else{
+        bill = 1200 +  10 * (U - 200);
+    }
+    return bill;
+    }
+    public static void main(String[] args){
+        Scanner sc = new Scanner(System.in);
+        System.out.println("Enter U:");
+        double U = sc.nextDouble();
+        double amt = electricityBillpayment(U);
+        System.out.println("Electricitybillpayment ="+amt);
+    }
+}
+
 
